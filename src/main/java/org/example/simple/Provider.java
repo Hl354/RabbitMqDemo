6118db7fe81d116debef949b71d5378939b6ff2a
admin <3415796710@qq.com>
2020-04-21 22:43:48 +0800

    持久化消息

--- src/main/java/org/example/simple/Provider.java
+++ src/main/java/org/example/simple/Provider.java
@@ -2,6 +2,7 @@ package org.example.simple;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
 import org.example.util.RabbitMqUtil;
 
 import java.io.IOException;
@@ -17,10 +18,11 @@ public class Provider {
         Connection connection = RabbitMqUtil.getInstance().newConnection();
         // 创建管道
         Channel channel = connection.createChannel();
-        // 参数依次的含义，1-队列名、2-是否持久化、3-是否独占、4-是否当队列为空时(连接已断开)删除队列、5-额为信息
+        // 参数依次的含义，1-队列名、2-是否持久化(队列)、3-是否独占、4-是否当队列为空时(连接已断开)删除队列、5-额为信息
         channel.queueDeclare("SimpleMode", false, false, false, null);
         // 参数依次的含义，1-交换机名称、2-路由(或者队列)名称、3-额为信息、4-信息字节
-        channel.basicPublish("", "SimpleMode", null, "SimpleMode".getBytes());
+        // MessageProperties.PERSISTENT_TEXT_PLAIN持久化消息
+        channel.basicPublish("", "SimpleMode", MessageProperties.PERSISTENT_TEXT_PLAIN, "SimpleMode".getBytes());
         // 关闭连接
         RabbitMqUtil.closeConnection(connection, channel);
     }
